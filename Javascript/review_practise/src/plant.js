7574a51ea7910279535bad87812afc9d967db5a1
Flora Davies <daviesfgo@gmail.com>
2021-04-27 18:09:31 +0100

    extracted wrong temperature case to separate function

--- Javascript/review_practise/src/plant.js
+++ Javascript/review_practise/src/plant.js
@@ -1,18 +1,20 @@
+let stem = "-";
 
-function plant(seed, water, fert, temp) {
-  const stem = "-";
+function plant(seed, water, fert, temp) {  
   if (tempCheck(temp)) {
     flowerUnit = `${stem.repeat(water)}` +  `${seed.repeat(fert)}`;
     return flowerUnit.repeat(water);
   } else {
-    return `${stem.repeat(water*water)}` +  `${seed}`
+    return wrongTemp(water, seed);
   }
-
 }
 
 function tempCheck(temp) {
   return temp >= 20 && temp <= 30;
 }
 
+function wrongTemp(water, seed){
+  return `${stem.repeat(water*water)}` +  `${seed}`;
+}
 
 module.exports = plant; 
