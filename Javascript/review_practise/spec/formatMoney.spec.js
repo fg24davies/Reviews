a6d3fac3dfbbacf7710c27ca5dbe07c820fd1f21
Flora Davies <daviesfgo@gmail.com>
2021-04-29 09:29:31 +0100

    hardcoded first passing test

---
+++ Javascript/review_practise/spec/formatMoney.spec.js
@@ -0,0 +1,7 @@
+const formatMoney = require('../src/formatMoney.js');
+
+describe('formatMoney', () => {
+  test('it turns takes 2dp number and returns in dollar and cents', () => {
+    expect(formatMoney(1.99)).toEqual ('$1.99')
+  });
+});
---
+++ Javascript/review_practise/src/formatMoney.js
@@ -0,0 +1,5 @@
+function formatMoney(number) {
+  return "$1.99";
+}
+
+module.exports = formatMoney;
