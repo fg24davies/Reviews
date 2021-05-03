37691628754849f82706739f7d5f46aa258b7e8a
Flora Davies <daviesfgo@gmail.com>
2021-05-03 17:10:40 +0100

    all tests passing

--- Javascript/review_practise/spec/zipWith.spec.js
+++ Javascript/review_practise/spec/zipWith.spec.js
@@ -14,4 +14,12 @@ describe('zipWith', () => {
     expect(zipWith(add, [1], [0])).toEqual([1]);
   });
 
+  test('takes a function and two arrays as arguemnts', () => {
+    expect(zipWith(add, [1,2], [0,2])).toEqual([1,4]);
+  });
+
+   test('takes a function and two arrays as arguemnts', () => {
+    expect(zipWith(add, [1,2], [2])).toEqual([3]);
+  });
+
 });
--- Javascript/review_practise/src/zipWith.js
+++ Javascript/review_practise/src/zipWith.js
@@ -1,8 +1,14 @@
 
 function zipWith(fn, array1, array2) {
   array = [];
-  array.push(fn(array1[0],array2[0]));
+  for (let i = 0; i < arrayLength(array1, array2); i++) {
+    array.push(fn(array1[i],array2[i]));
+  }
   return array;
+}
+
+function arrayLength(array1, array2) {
+  return Math.min(array1.length, array2.length)
 }
 
 module.exports = zipWith
