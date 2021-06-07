6ff034b8a78ab03c58513697bed22d67ca7baa11
Flora Davies <daviesfgo@gmail.com>
2021-06-07 11:10:12 +0100

    All tests passing and refactored

--- Javascript/review_practise/src/removeSmallest.js
+++ Javascript/review_practise/src/removeSmallest.js
@@ -1,17 +1,16 @@
 function removeSmallest(inputArray) {
+  // empty array case
   if (inputArray.length === 0) {
     return inputArray;
   }
+  // find min of array
   var min = Math.min(...inputArray);
 
   //make a copy of array
   let outputArray = inputArray.slice();
-  outputArray.splice(outputArray.indexOf(min), 1);
 
-  console.log(min);
-  //console.log(min);
-  //const outputArray = inputArray.filter((number) => number > min);
-  console.log(outputArray);
+  // delete the first min number
+  outputArray.splice(outputArray.indexOf(min), 1);
   return outputArray;
 }
 
