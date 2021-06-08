7c3c030f8d678728b48872cdec6c89bcab559b73
Flora Davies <daviesfgo@gmail.com>
2021-06-08 11:40:22 +0100

    Refactored

--- Javascript/review_practise/src/reverseArray.js
+++ Javascript/review_practise/src/reverseArray.js
@@ -1,5 +1,13 @@
-function reverseArray() {
-  return [3, 2];
+function reverseArray(array) {
+  let endIndex = array.length - 1;
+  // console.log(endIndex);
+
+  for (let i = 0; i < endIndex; i++) {
+    let temp = array[i];
+    array[i] = array[endIndex - i];
+    array[endIndex - i] = temp;
+  }
+  return array;
 }
 
 module.exports = reverseArray;
