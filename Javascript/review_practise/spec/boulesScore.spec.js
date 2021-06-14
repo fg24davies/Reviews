ce997234b113f09da351db8c26c31517212bbd91
Flora Davies <daviesfgo@gmail.com>
2021-06-14 11:39:58 +0100

    Hardcoded one ball scoring one point

--- Javascript/review_practise/spec/boulesScore.spec.js
+++ Javascript/review_practise/spec/boulesScore.spec.js
@@ -1,13 +1,21 @@
 const boulesScore = require("../src/boulesScore");
 
 describe("boulesScore", () => {
-  test("red scores 1 being closest in a line", () => {
+  test("red scores if only one red ball thrown and jack", () => {
     expect(
       boulesScore([
-        { type: "black", distance: [70, 0] },
         { type: "red", distance: [85, 0] },
         { type: "jack", distance: [80, 0] },
       ])
     ).toEqual("Red scores 1");
+  });
+
+  test("black scores if only one black ball thrown and jack", () => {
+    expect(
+      boulesScore([
+        { type: "black", distance: [85, 0] },
+        { type: "jack", distance: [80, 0] },
+      ])
+    ).toEqual("Black scores 1");
   });
 });
--- Javascript/review_practise/src/boulesScore.js
+++ Javascript/review_practise/src/boulesScore.js
@@ -1,5 +1,9 @@
 function boulesScore(balls) {
-  return "Red scores 1";
+  if (balls[0].type === "red") {
+    return "Red scores 1";
+  } else if (balls[0].type === "black") {
+    return "Black scores 1";
+  }
 }
 
 module.exports = boulesScore;
