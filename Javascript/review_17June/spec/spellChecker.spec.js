ebdec876beb14ab37434f9448eca7b6f447bf82c
Flora Davies <daviesfgo@gmail.com>
2021-06-17 12:25:41 +0100

    Returns 'fox' when passed 'fox'

--- Javascript/review_17June/spec/spellChecker.spec.js
+++ Javascript/review_17June/spec/spellChecker.spec.js
@@ -4,4 +4,8 @@ describe("spellChecker", () => {
   test("it returns 'the' when passed 'the'", () => {
     expect(spellChecker("the")).toEqual("the");
   });
+
+  test("it returns 'fox' when passed 'fox'", () => {
+    expect(spellChecker("fox")).toEqual("fox");
+  });
 });
--- Javascript/review_17June/src/spellChecker.js
+++ Javascript/review_17June/src/spellChecker.js
@@ -1,5 +1,11 @@
-function spellChecker() {
-  return "the";
+function spellChecker(sentence) {
+  const wordBank = ["the", "fox"];
+
+  for (let i = 0; i < wordBank.length; i++) {
+    if (sentence === wordBank[i]) {
+      return sentence;
+    }
+  }
 }
 
 module.exports = spellChecker;
