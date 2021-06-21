06a380b6366b2178a3dc7c34e715ff678e58a40f
Flora Davies <daviesfgo@gmail.com>
2021-06-21 11:30:40 +0100

    Calculates digital root for 2digit number

--- Javascript/review_practise/spec/digitalRoot.spec.js
+++ Javascript/review_practise/spec/digitalRoot.spec.js
@@ -8,4 +8,8 @@ describe("digitalRoot", () => {
   test("given 2 it returns 2", () => {
     expect(digitalRoot(2)).toEqual(2);
   });
+
+  test("given 21 it returns 3", () => {
+    expect(digitalRoot(21)).toEqual(3);
+  });
 });
--- Javascript/review_practise/src/digitalRoot.js
+++ Javascript/review_practise/src/digitalRoot.js
@@ -1,5 +1,11 @@
 function digitalRoot(number) {
-  return number;
+  let digits = number.toString().split("");
+  let integers = digits.map((digit) => parseInt(digit));
+
+  let sum = integers.reduce((a, b) => a + b);
+  console.log(integers);
+  console.log(sum);
+  return sum;
 }
 
 module.exports = digitalRoot;
