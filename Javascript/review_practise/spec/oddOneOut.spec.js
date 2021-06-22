991d308207a9a6cd7ec2e02660c672712bccdfc9
Flora Davies <daviesfgo@gmail.com>
2021-06-22 13:00:04 +0100

    Passes for 3 numbers in an array and odd one out is at the end

--- Javascript/review_practise/spec/oddOneOut.spec.js
+++ Javascript/review_practise/spec/oddOneOut.spec.js
@@ -4,4 +4,8 @@ describe("oddOneOut", () => {
   test("it returns the non-matching number in array of 3 numbers", () => {
     expect(oddOneOut([1, 1, 2])).toEqual(2);
   });
+
+  test("it returns the non-matching number in array of 3 numbers", () => {
+    expect(oddOneOut([1, 1, 3])).toEqual(3);
+  });
 });
--- Javascript/review_practise/src/oddOneOut.js
+++ Javascript/review_practise/src/oddOneOut.js
@@ -1,5 +1,12 @@
-const oddOneOut = () => {
-  return 2;
+const oddOneOut = (numbersArray) => {
+  for (let i = 1; i < numbersArray.length; i++) {
+    if (
+      numbersArray[i] !== numbersArray[i + 1] &&
+      numbersArray[i] !== numbersArray[i - 1]
+    ) {
+      return numbersArray[i];
+    }
+  }
 };
 
 module.exports = oddOneOut;
