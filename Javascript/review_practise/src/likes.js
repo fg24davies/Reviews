cfd778d97eea40a6970069163c1858dd29e76692
Flora Davies <daviesfgo@gmail.com>
2021-06-22 13:47:37 +0100

    Switch Statement refactored

--- Javascript/review_practise/src/likes.js
+++ Javascript/review_practise/src/likes.js
@@ -1,10 +1,14 @@
 const likes = (names) => {
-  if (names.length === 0) {
-    return "no one likes this";
-  } else if (names.length === 1) {
-    return `${names[0]} likes this`;
-  } else {
-    return `${names[0]} and ${names[1]} like this`;
+  let pluralLike = "like this";
+  let singularLike = "likes this";
+
+  switch (names.length) {
+    case 0:
+      return `no one ${singularLike}`;
+    case 1:
+      return `${names[0]} ${singularLike}`;
+    case 2:
+      return `${names[0]} and ${names[1]} ${pluralLike}`;
   }
 };
 
