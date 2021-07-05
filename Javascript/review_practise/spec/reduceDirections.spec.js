9c1e8bf9add81317c831796660b6ace726ab8cf3
Flora Davies <daviesfgo@gmail.com>
2021-07-05 17:24:45 +0100

    Refactored tests for readability

--- Javascript/review_practise/spec/reduceDirections.spec.js
+++ Javascript/review_practise/spec/reduceDirections.spec.js
@@ -1,11 +1,8 @@
 const reduceDirections = require("../src/reduceDirections");
 
 describe("reduceDirections", () => {
-  test("it returns North if only direction of North given ", () => {
+  test("it returns that direction if only one direction  given ", () => {
     expect(reduceDirections(["NORTH"])).toEqual(["NORTH"]);
-  });
-
-  test("it returns West if only direction of West given ", () => {
     expect(reduceDirections(["WEST"])).toEqual(["WEST"]);
   });
 });
