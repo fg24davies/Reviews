ef4f48f62d848165d5badc26a35531e4365ec576
Flora Davies <daviesfgo@gmail.com>
2021-07-09 13:53:19 +0100

    Cancels south and north

--- Javascript/review_practise/spec/reduceDirections.spec.js
+++ Javascript/review_practise/spec/reduceDirections.spec.js
@@ -6,7 +6,11 @@ describe("reduceDirections", () => {
     expect(reduceDirections(["WEST"])).toEqual(["WEST"]);
   });
 
-  test("it returns that direction if only one direction given ", () => {
+  test("it returns no direction if they cancel eachother out ", () => {
     expect(reduceDirections(["SOUTH", "NORTH"])).toEqual([]);
+  });
+
+  test("it returns the one direction left over if the othes cancel eachother out ", () => {
+    expect(reduceDirections(["SOUTH", "NORTH", "WEST"])).toEqual(["WEST"]);
   });
 });
--- Javascript/review_practise/src/reduceDirections.js
+++ Javascript/review_practise/src/reduceDirections.js
@@ -1,9 +1,15 @@
 const reduceDirections = (fullDirections) => {
   if (fullDirections.length === 1) {
     return fullDirections;
-  } else {
-    return [];
   }
+
+  fullDirections.forEach((direction, index) => {
+    if (direction === "SOUTH" && fullDirections[index + 1] === "NORTH") {
+      fullDirections.splice(index, 2);
+    }
+  });
+  console.log(fullDirections);
+  return fullDirections;
 };
 
 module.exports = reduceDirections;
